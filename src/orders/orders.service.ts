c7a98f4c1844621c005bb0ccf665c357477541d7
Dieguidev <diegogaraycullas@gmail.com>
2024-05-04 07:37:51 -0500

    se agrega todo el createOrder.dto.ts para traer los datos del api-gateway

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,7 +1,8 @@
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
-import { CreateOrderDto } from './dto/create-order.dto';
+
 
 import { PrismaClient } from '@prisma/client';
+import { CreateOrderDto } from './dto';
 
 @Injectable()
 export class OrdersService extends PrismaClient implements OnModuleInit{
@@ -14,7 +15,7 @@ export class OrdersService extends PrismaClient implements OnModuleInit{
   }
 
   create(createOrderDto: CreateOrderDto) {
-    return 'This action adds a new order';
+    return createOrderDto;
   }
 
   findAll() {
