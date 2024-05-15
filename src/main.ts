37043702e26708d1b45ee3db0803fccb0d42ed0b
Dieguidev <diegogaraycullas@gmail.com>
2024-05-15 17:57:33 -0500

    configurado todo para levantar con docker

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const logger = new Logger('order-ms')
+
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
     transport: Transport.NATS,
     options: {
